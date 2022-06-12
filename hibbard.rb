285b0104c438ee7906a7ce519344f006a040785a
Hennie Bruurmijn <hlm.bruurmijn@gmail.com>
2022-06-12 22:57:14 +0200

    Write Node class with Comparable mixin

--- hibbard.rb
+++ hibbard.rb
@@ -2,3 +2,10 @@
 
 require_relative './lib/node'
 require_relative './lib/tree'
+
+node1 = Node.new(10)
+node2 = Node.new(20)
+node3 = Node.new(30)
+
+p node1 < node2
+p node2.between?(node1, node3)
--- lib/node.rb
+++ lib/node.rb
@@ -1,1 +1,18 @@
 # frozen_string_literal: true
+
+# Node class used to create a new node for the BST.
+class Node
+  include Comparable
+
+  attr_accessor :data, :left, :right
+
+  def initialize(data)
+    @left = nil
+    @data = data
+    @right = nil
+  end
+
+  def <=>(other)
+    data <=> other.data
+  end
+end
